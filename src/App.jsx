3e4328d9760e42d996ba65baeaebe30b70ba723b
abhishekkhatale <abhishekkhatale007@gmail.com>
2025-04-02 21:52:18 +0530

    verseljson

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Route path="/login" element={<LoginModal />} />
       <Route path="/signup" element={<SignupModal />} />
       <Route path="/dashboard" element={<Dashboard/>}/>
-
+      
     </Routes>
   );
 }
